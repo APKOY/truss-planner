088a9085794201b4dc5ca498a0bfc94a419e6083
APKOY <alexsander2006.ribeiro@gmail.com>
2026-04-22 00:55:47 -0300

    melhorias

--- truss-planner/vite.config.js
+++ truss-planner/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Aumenta o limite de aviso para 2000 kB (2 MB)
+  }
 })
